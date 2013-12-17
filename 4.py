0929efa5aa7286ab906a848d77381e06b101951f
jezinka <jezina1989@gmail.com>
2013-12-17 21:53:14 +0100

    assert

--- 4.py
+++ 4.py
@@ -1,4 +1,9 @@
 __author__ = 'jezinka'
 
-print max([i * j for j in range(100, 1000) for i in range(100, 1000) if str(i * j) == str(i * j)[::-1]])
 
+def ex4(limit):
+    return max([i * j for j in range(limit/10, limit) for i in range(limit/10, limit) if str(i * j) == str(i * j)[::-1]])
+
+assert ex4(100) == 9009
+
+print ex4(1000)
