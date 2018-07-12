5a43186b221af99bef5b691cd36e0be5c8b8f637
jezinka <jezina1989@gmail.com>
2018-07-12 19:00:46 +0200

    ex 8 - refactor for blogging

--- 8.py
+++ 8.py
@@ -3,33 +3,42 @@ large_number = 73167176531330624919225119674426574742355349194934969835203127745
 large_number_array = list(map(int, str(large_number)))
 
 
-def find_max_product(k):
-    max_product = 1
-    for i in range(0, k):
-        max_product *= large_number_array[i]
+def find_max_product(window_size):
+    i = skip_zeros(0, window_size)
+    max_product = count_initial_product(i, window_size)
 
-    i = k
+    i += window_size
     window_product = max_product
-    while i + k <= len(large_number_array):
+    while i + window_size <= len(large_number_array):
         next_item = large_number_array[i]
-        previous_item = large_number_array[i - k]
+        previous_item = large_number_array[i - window_size]
         if next_item is not 0:
             window_product = window_product // previous_item * next_item
             i += 1
         else:
-            i += 1
-            while 0 in large_number_array[i:i + k]:
-                zero_index = large_number_array[i:i + k].index(0)
-                i += zero_index + 1
-            window_product = 1
-            if i + k <= len(large_number_array):
-                for j in range(i, i + k):
-                    window_product *= large_number_array[j]
+            i = skip_zeros(i, window_size)
+
+            if i + window_size <= len(large_number_array):
+                window_product = count_initial_product(i, window_size)
             else:
                 break
-            i += k
+            i += window_size
         max_product = max(window_product, max_product)
     return max_product
+
+
+def skip_zeros(i, window_size):
+    while 0 in large_number_array[i:i + window_size]:
+        zero_index = large_number_array[i:i + window_size].index(0)
+        i += zero_index + 1
+    return i
+
+
+def count_initial_product(i, window_size):
+    window_product = 1
+    for j in range(i, i + window_size):
+        window_product *= large_number_array[j]
+    return window_product
 
 
 assert find_max_product(4) == 5832
